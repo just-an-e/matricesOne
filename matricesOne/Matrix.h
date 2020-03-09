d4dc5a53b2493ab7d6e2643ddae1e1229966bdd1
VoidXpulse <voidxpulse@gmail.com>
2020-03-09 15:58:01 -0400

    finally got matrix.h working

--- matricesOne/Matrix.h
+++ matricesOne/Matrix.h
@@ -3,12 +3,22 @@
 
 using namespace std;
 
+template <typename T>
 class Matrix {
-public:
-	Matrix(int vLengthOne, int vLengthTwo);
-	void setCell(int x, int y, int num);
-	int getCell(int x, int y);
-
 private:
-	vector <vector<int>> mat;
+	vector <vector<T>> mat;
+public:
+	Matrix <T> (int row, int column) {
+		mat.resize(row, vector<T>(column));
+		for (int i = 0; i < row; i++)
+			mat[i].resize(column);
+	}
+	void setCell(int x, int y, T num) {
+		mat[x][y] = num;
+	}
+	int getCell(int x, int y) {
+		return mat[x][y];
+	}
+
+
 };
--- matricesOne/matricesOne.cpp
+++ matricesOne/matricesOne.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include "Matrix.h"
 
+extern template class Matrix<double>;
+//extern template class Matrix<string>;
+
 using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	Matrix <double> numbers = Matrix <double>(1, 5);
+	numbers.setCell(0, 0, 1);
+	numbers.setCell(0, 1, 100);
 }
 
